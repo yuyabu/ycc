949e067e1dc067f6a79826e945c559dd9d7a7b83
yuyabu <yuyabushita@gmail.com>
2019-07-08 20:52:56 +0900

    improve error message

--- ycc.c
+++ ycc.c
@@ -25,6 +25,22 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 /// エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -56,7 +72,7 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str,"数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -99,7 +115,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p,"トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -107,6 +123,9 @@ Token *tokenize(char *p) {
 }
 
 int main(int argc, char **argv) {
+  
+  user_input=argv[1];	
+	
   if (argc != 2) {
     error("引数の個数が正しくありません");
     return 1;
